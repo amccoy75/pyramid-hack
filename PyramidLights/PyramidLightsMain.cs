3ee62ae892de75bee3ed21ab8bf2f8ec898527dc
PGAC\ntismk01 <mkemp@thegeneral.com>
2017-01-30 21:15:58 -0600

    Added functionality to send tweet at spreadsheet time

--- PyramidLights/PyramidLightsMain.cs
+++ PyramidLights/PyramidLightsMain.cs
@@ -1,31 +1,39 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 
 
     class PyramidLightsMain
     {
-        static void Main(string[] args)
+
+    static void Main(string[] args)
         {
-            cConfigurationHelper configHelper = new cConfigurationHelper();
+
+            cConfigurationHelper  configHelper = new cConfigurationHelper();
             cTwitterHelper twitterHelper = new cTwitterHelper(configHelper);
             cQuestion question = new cQuestion(configHelper);
-       
+
             //get question from googledocs
             question.getQuestion();
+            if (question.startDateTime > DateTime.Now)
+            {
+                TimeSpan offset;
+                offset = question.startDateTime - DateTime.Now;
+                Thread.Sleep(offset);
+                question.getQuestion();
+        }
             if (question.questionText != null)
-             {
-            //tweet question
-            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+            {
+                //tweet question
+                TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
 
-            long questionTweetID = twitterHelper.sendTweet(question.questionText + " " + myTI.ToTitleCase(question.answer1) +
-                                                                ", " + question.answer2 + ", " + question.answer3 +
-                                                                ", or " + question.answer4 + "?" + " " + DateTime.Now.ToString());
+                long questionTweetID = twitterHelper.sendTweet(question.questionText + " " + myTI.ToTitleCase(question.answer1) +
+                                                                    ", " + question.answer2 + ", " + question.answer3 +
+                                                                    ", or " + question.answer4 + "?" + " " + DateTime.Now.ToString());
                 //listen for responses 
                 twitterHelper.listenAndProcess(questionTweetID, question);
-             }
-           
-
-        }
-
+            }
+    }
+  
 }
 
