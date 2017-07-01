0b21677d6c2e24be546f584c432bd5cc8331ad7f
ashokbalusu <ashokbalusu@hotmail.com>
2017-07-01 16:21:29 -0500

    Added daily question reloads

--- PyramidLights/PyramidLightsMain.cs
+++ PyramidLights/PyramidLightsMain.cs
@@ -1,39 +1,79 @@
 ﻿using System;
 using System.Globalization;
 using System.Threading;
+using System.Threading.Tasks;
 
 
     class PyramidLightsMain
     {
 
     static void Main(string[] args)
+    {
+
+        cConfigurationHelper  configHelper = new cConfigurationHelper();
+
+        try
         {
-
-            cConfigurationHelper  configHelper = new cConfigurationHelper();
-            cTwitterHelper twitterHelper = new cTwitterHelper(configHelper);
-            cQuestion question = new cQuestion(configHelper);
-
-            //get question from googledocs
-            question.getQuestion();
-            if (question.startDateTime > DateTime.Now)
+            while (true)
             {
-                TimeSpan offset;
-                offset = question.startDateTime - DateTime.Now;
-                Thread.Sleep(offset);
+                DateTime dtStartToday = DateTime.Now;
+                DateTime dtResetDay = DateTime.Now;
+                cTwitterHelper twitterHelper = new cTwitterHelper(configHelper);
+                cQuestion question = new cQuestion(configHelper);
+
+                //get question from googledocs
                 question.getQuestion();
-        }
-            if (question.questionText != null)
-            {
-                //tweet question
-                TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
-                if (!question.tweetTooLong())
+
+                while (true)
                 {
-                    long questionTweetID = twitterHelper.sendTweet(question.tweetString + DateTime.Now.ToString());
-                    //listen for responses 
-                    twitterHelper.listenAndProcess(questionTweetID, question);
+
+                    if (question.startDateTime > DateTime.Now)
+                    {
+                        TimeSpan offset;
+                        offset = question.startDateTime - DateTime.Now;
+                        Thread.Sleep(offset);
+                        question.getQuestion();
+                    }
+                    if (question.questionText != null)
+                    {
+                        //tweet question
+                        TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+                        if (!question.tweetTooLong())
+                        {
+                            long questionTweetID = twitterHelper.sendTweet(question.tweetString + " " + DateTime.Now.ToString());
+
+                            //var twitterTask = Task.Run(async () => await cTwitterHelper.ProcessTweet(questionTweetID, question));
+                            //listen for responses 
+                            twitterHelper.listenAndProcess(questionTweetID, question);
+
+                        }
+
+                    }
+                    dtResetDay = DateTime.Now;
+
+                    if (dtStartToday.AddDays(1) > dtResetDay && dtStartToday.Day != dtResetDay.Day)
+                    {
+                        break;
+                    }
                 }
+
             }
+        }
+        catch (Exception Ex)
+        {
+            System.Diagnostics.EventLog.WriteEntry("PyramidLightsError", Ex.ToString(), System.Diagnostics.EventLogEntryType.Error);
+
+            try
+            {
+                PyramidLights.Classes.cMail.SendEmail(configHelper, Ex.Message);
+            }
+            catch(Exception ignoreEx)
+            {
+                System.Diagnostics.EventLog.WriteEntry("PyramidLightsError-Mail", ignoreEx.ToString(), System.Diagnostics.EventLogEntryType.Error);
+            }
+        }
     }
-  
+
 }
 
